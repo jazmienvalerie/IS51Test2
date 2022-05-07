89e0376cd78a0c5b592932398183d9591e0a9061
Jazmien <jazmienb247@gmail.com>
2022-05-06 19:25:39 -0700

    finished structured-english

---
+++ test_2.py
@@ -0,0 +1,31 @@
+"""
+Laymen's terms (structure english)
+
+The program will use Final.txt to determine and display the following.
+It will display the number of grades. It will also display the average grade.
+Lastly, it will display the percentage of grades that are higher than the average grade.
+
+We will use the numbers in Final.txt to calculate the percentage of grades that 
+are above the average grade. 
+
+Num_of_grades will be 24.
+Average_grade will be 83.25
+
+Percentage_of_grades_above_average will be 54.17%
+
+Add all 24 grades to find the sum.
+Divide the sum by 24 (number of grades).
+The given output will display the overall average. 
+
+Add all grades that are higher than the average. 
+Divide the sum by 100.
+The given output will display the percentage of grades that are 
+above the average grade.
+
+"""
+
+
+
+
+
+
